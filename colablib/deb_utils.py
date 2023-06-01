5da81484f6a6cf58d44cf622dda2d0013630d77e
Linaqruf <furqanil.taqwa@gmail.com>
2023-06-01 18:45:04 +0700

    fix: ubuntu deps still not working

--- colablib/deb_utils.py
+++ colablib/deb_utils.py
@@ -16,24 +16,24 @@ def ubuntu_deps(url, dst, desc=None):
         dst (str): The directory to extract to and install the .deb files from.
     """
     os.makedirs(dst, exist_ok=True)
-    filename = get_filename(url)
+    filename  = get_filename(url)
+    
     response = requests.get(url, stream=True)
     response.raise_for_status()
-
-    with open(filename, "wb") as file:
+    
+    with open(filename, 'wb') as file:
         for chunk in response.iter_content(chunk_size=8192):
-            if chunk:
-                file.write(chunk)
-
+            file.write(chunk)
+            
     with zipfile.ZipFile(filename, "r") as deps:
         deps.extractall(dst)
 
     if desc is None:
         desc = cprint("Installing", color="green", tqdm_desc=True)
 
-    for file in tqdm(os.listdir(dst), desc=desc):
-        if file.endswith(".deb"):
-            subprocess.run(["dpkg", "-i", os.path.join(dst, file)], stdout=subprocess.DEVNULL, check=True)
-
+    deb_files = [os.path.join(dst, f) for f in os.listdir(dst) if f.endswith('.deb')]
+    for deb_file in tqdm(deb_files, desc=desc):
+        os.system(f'dpkg -i {deb_file}')
+        
     os.remove(filename)
     shutil.rmtree(dst)
