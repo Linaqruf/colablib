1e092105ed605edbe4fcde60784b4dfea129e523
Linaqruf <furqanil.taqwa@gmail.com>
2023-06-03 16:24:53 +0700

    feat: v0.1.7

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='colablib',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
+    install_requires=[
+        'safetensors==0.2.6',
+        'requests==2.28.2',
+        'tqdm==4.65.0',
+    ],
     author='Furqanil Taqwa',
     author_email='furqanil.taqwa@gmail.com',
     description='A utility library for Google Colab',
