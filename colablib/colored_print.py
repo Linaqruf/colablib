9222948554d14f89a61ae6b012b60deb533bb9d0
Linaqruf <furqanil.taqwa@gmail.com>
2023-06-03 10:18:22 +0700

    fix: add timezone for cprint

--- colablib/colored_print.py
+++ colablib/colored_print.py
@@ -1,6 +1,38 @@
 import datetime
+import pytz
 
-def cprint(*args, color="default", style="normal", bg_color=None, reset=True, timestamp=False, line=None, tqdm_desc=False, timestamp_format='%Y-%m-%d %H:%M:%S', prefix=None, suffix=None):
+color_codes = {
+    "default"      : "\033[0m",
+    "black"        : "\033[0;30m",
+    "red"          : "\033[0;31m",
+    "green"        : "\033[0;32m",
+    "yellow"       : "\033[0;33m",
+    "blue"         : "\033[0;34m",
+    "purple"       : "\033[0;35m",
+    "cyan"         : "\033[0;36m",
+    "white"        : "\033[0;37m",
+    "flat_red"     : "\033[38;2;204;102;102m",
+    "flat_yellow"  : "\033[38;2;255;204;0m",
+    "flat_blue"    : "\033[38;2;0;102;204m",
+    "flat_purple"  : "\033[38;2;153;51;255m",
+    "flat_orange"  : "\033[38;2;255;153;0m",
+    "flat_green"   : "\033[38;2;0;204;102m",
+    "flat_gray"    : "\033[38;2;128;128;128m",
+    "flat_cyan"    : "\033[38;2;0;255;255m",
+    "flat_pink"    : "\033[38;2;255;0;255m",
+}
+
+style_codes = {
+    "normal"      : "\033[0m",
+    "bold"        : "\033[1m",
+    "italic"      : "\033[3m",
+    "underline"   : "\033[4m",
+    "blink"       : "\033[5m",
+    "inverse"     : "\033[7m",
+    "strikethrough": "\033[9m",
+}
+
+def cprint(*args, color="default", style="normal", bg_color=None, reset=True, timestamp=False, line=None, tqdm_desc=False, timestamp_format='%Y-%m-%d %H:%M:%S', prefix=None, suffix=None, timezone=None):
     """
     Prints colored text in the console.
 
@@ -16,41 +48,11 @@ def cprint(*args, color="default", style="normal", bg_color=None, reset=True, ti
         timestamp_format : The format of the timestamp if timestamp is True. Default is '%Y-%m-%d %H:%M:%S'.
         prefix           : Optional prefix for the text. Default is None.
         suffix           : Optional suffix for the text. Default is None.
+        timezone         : The timezone to use for the timestamp. If None, the local timezone will be used.
 
     Returns:
         None
     """
-    color_codes = {
-        "default"      : "\033[0m",
-        "black"        : "\033[0;30m",
-        "red"          : "\033[0;31m",
-        "green"        : "\033[0;32m",
-        "yellow"       : "\033[0;33m",
-        "blue"         : "\033[0;34m",
-        "purple"       : "\033[0;35m",
-        "cyan"         : "\033[0;36m",
-        "white"        : "\033[0;37m",
-        "flat_red"     : "\033[38;2;204;102;102m",
-        "flat_yellow"  : "\033[38;2;255;204;0m",
-        "flat_blue"    : "\033[38;2;0;102;204m",
-        "flat_purple"  : "\033[38;2;153;51;255m",
-        "flat_orange"  : "\033[38;2;255;153;0m",
-        "flat_green"   : "\033[38;2;0;204;102m",
-        "flat_gray"    : "\033[38;2;128;128;128m",
-        "flat_cyan"    : "\033[38;2;0;255;255m",
-        "flat_pink"    : "\033[38;2;255;0;255m",
-    }
-
-    style_codes = {
-        "normal"      : "\033[0m",
-        "bold"        : "\033[1m",
-        "italic"      : "\033[3m",
-        "underline"   : "\033[4m",
-        "blink"       : "\033[5m",
-        "inverse"     : "\033[7m",
-        "strikethrough": "\033[9m",
-    }
-    
     if color not in color_codes:
         raise ValueError(f"Invalid color value '{color}'. Available options are: {', '.join(color_codes.keys())}")
 
@@ -69,14 +71,14 @@ def cprint(*args, color="default", style="normal", bg_color=None, reset=True, ti
 
     if prefix:
         formatted_text = str(prefix) + formatted_text
-        
+
     if suffix:
         formatted_text = formatted_text + str(suffix)
 
     if timestamp:
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(pytz.timezone(timezone) if timezone else pytz.timezone('UTC'))
         formatted_text = f"[{now.strftime(timestamp_format)}] {formatted_text}"
-        
+
     if line:
         formatted_text = "\n" * line + formatted_text
 
@@ -84,5 +86,3 @@ def cprint(*args, color="default", style="normal", bg_color=None, reset=True, ti
         return color_start + formatted_text
     else:
         print(color_start + formatted_text + color_end)
-
-
