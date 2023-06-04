1a61b27f39e626d99d291f8b79986a45c6ef38fc
Linaqruf <furqanil.taqwa@gmail.com>
2023-06-04 16:28:54 +0700

    fix missing comma

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
         'requests==2.28.2',
         'tqdm==4.65.0',
         'PyYAML==6.0',
-        'gdown==4.7.1'
+        'gdown==4.7.1',
         'toml==0.10.2',
         'rarfile==4.0',
-        'xmltodict==0.13.0'
+        'xmltodict==0.13.0',
     ],
     author='Furqanil Taqwa',
     author_email='furqanil.taqwa@gmail.com',
